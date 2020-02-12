10f4e83ef793358fbd74e772735a602714b7adea
max-student <max-student@maxtrain.com>
2020-02-12 16:32:27 -0500

    Add project files.

---
+++ BankingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using BankingLibrary;
+using System;
+
+namespace BankingApp
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            var chk = new Account();
+            chk.Depoist(100);
+            chk.Withdraw(75);
+
+
+
+            }
+        }
+    }
---
+++ BankingLibrary/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankingLibrary
+    {
+
+
+    public class Account
+        {
+        public decimal Balance { get; set; }
+        public string AcctNbr { get; set; }
+        public string Description { get; set; }
+
+
+        public void Depoist(decimal amount)
+            {
+            Balance += amount; }
+
+        public void Withdraw(decimal amount)
+            {
+            Balance -= amount; }
+
+                public void Transfer(decimal amount, Account toaccount, Account fromaccount)
+            {
+            fromaccount.Withdraw(amount);
+            toaccount.Depoist(amount);
+            }
+
+            }
+        }       
+    
